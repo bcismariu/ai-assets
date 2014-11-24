291e6187584827c62a09f53c33fc1a84bdc3cc2c
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2014-11-24 14:18:44 +0200

    meniul este accesibil si pe mobil. se marcheaza ca activa in meniu pagina curenta

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -3,12 +3,28 @@
 $(function() {
 
 	$('.dropdown > a').click(function(){
-		console.log('clicked');
-		if(!$(this).next().is(':visible'))
-			$(this).next().slideDown('fast');
-		else
-			$(this).next().slideUp('fast');	
+		aiMenuSlide($(this).next());
 		return false;
 	});
 
+	// activam in meniu pagina curenta
+	var scriptUrl = window.location.origin + window.location.pathname;
+	var element = $('a[href="' + scriptUrl + '"]');
+	element.parent().addClass('active');
+	// verific daca am de a face cu un submeniu care trebuie afisat
+	var upperMenu = element.parent().parent().parent();
+	if (upperMenu.hasClass('dropdown')) {
+		var menus = upperMenu.find('ul');
+		$(menus).each(function () {
+			aiMenuSlide($(this));
+		});
+	}
 });
+
+function aiMenuSlide(element) {
+	console.log(element);
+	if (!element.is(':visible'))
+		element.slideDown('fast');
+	else
+		element.slideUp('fast');
+}
